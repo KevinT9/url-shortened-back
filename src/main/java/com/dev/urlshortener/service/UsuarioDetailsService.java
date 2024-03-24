810dc7a72a7eaf71c8ffbdd5fb1a2867bd04e186
Kevin <kevin_junior1999@hotmail.com>
2024-03-24 04:20:41 -0500

    add SecurityConfig

--- src/main/java/com/dev/urlshortener/service/UsuarioDetailsService.java
+++ src/main/java/com/dev/urlshortener/service/UsuarioDetailsService.java
@@ -2,7 +2,6 @@ package com.dev.urlshortener.service;
 
 import com.dev.urlshortener.entity.UserEntity;
 import com.dev.urlshortener.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
