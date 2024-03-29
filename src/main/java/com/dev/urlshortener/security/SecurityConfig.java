739d9ef7b7e40d9f439a9bd05c4fdc4a933bc50e
Kevin <kevin_junior1999@hotmail.com>
2024-03-28 19:35:31 -0500

    add Cors

--- src/main/java/com/dev/urlshortener/security/SecurityConfig.java
+++ src/main/java/com/dev/urlshortener/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -32,6 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .exceptionHandling(ex -> ex.accessDeniedPage("/access-denied"))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/registrarse", "/*", "/api/url/user/**").permitAll()
@@ -58,6 +64,19 @@ public class SecurityConfig {
 //                .exceptionHandling()
 //                .accessDeniedPage("/access-denied");
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Permitir solicitudes desde cualquier origen
+        configuration.addAllowedMethod("*"); // Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+        configuration.addAllowedHeader("*"); // Permitir todos los encabezados
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplicar la configuración CORS a todas las rutas
+
+        return source;
     }
 
     @Bean
